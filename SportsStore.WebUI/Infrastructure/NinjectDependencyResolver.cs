32ab2d8cb92f2384b45988a3dcb1bdea3736d7c1
Simon Touitou <totogate@gmail.com>
2016-12-15 22:54:06 +0200

    WebUI Added

---
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,47 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+         private void AddBindings()
+        {
+           kernel.Bind<IProductRepository>().To<EFProductRepository>();
+            #region Mock_Demo_Before_Repository_Bindings_Not_Relevant_Anymore_Just_For_Learning_Purposes
+            //Before I have a db I just present data based on mock the IProductRepository Returns
+            //Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            //mock.Setup(m => m.Products).Returns(new List<Product>
+            //{
+            //    new Product { Name = "Football", Price = 25 },
+            //    new Product { Name = "Surf board", Price = 179 },
+            //    new Product { Name = "Running shoes", Price = 95 }
+            //});
+            //kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            #endregion
+        }
+    }
+}
